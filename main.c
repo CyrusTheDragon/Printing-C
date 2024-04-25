480c3f3e074ac1904c3ea9159d9f9d8a2fc9881f
CyrusTheDragon <82590539+CyrusTheDragon@users.noreply.github.com>
2024-04-25 18:05:52 -0400

    First Code

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void oh_shit() {
+	printf("Okay so now we are getting somewhere");
+}
+
+void no_stoppy_woppy() {
+	printf("Im learning wowie woieeeow no stoppy woppy on TOP\n");
+}
+
+int main() {
+// This prints hello world and some random bullshit
+  printf("Hello World!\n");
+  printf("me when the IM DYING\n"); no_stoppy_woppy();
+
+	oh_shit();
+
+  return 0; 
+} 
+/* what 
+makes sense i suppose*/
