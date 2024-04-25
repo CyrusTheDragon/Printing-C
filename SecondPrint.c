9ebb74aef578cdc96c2d3cceb2b9c37c3e67fe99
CyrusTheDragon <82590539+CyrusTheDragon@users.noreply.github.com>
2024-04-25 10:15:35 -0400

    Added Second time making Print Code

---
+++ SecondPrint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* This goes to show that I remember everything from last night (maybe)
+*/
+void secondary_action() { printf("uhhhh returb test\\"); }
+
+void me_when() { printf("making sure i can remember how to do all of this\n"); }
+
+int wow() { printf("\n\nAll Finished!"); return 0;
+
+}
+
+int main() { 
+ printf("wowwie zowwie\n"); me_when(); secondary_action(); 
+
+    return wow();
+}
